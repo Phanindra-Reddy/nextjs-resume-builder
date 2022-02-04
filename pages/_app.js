6d10291dd188933ec0de537591e9fcc26daf5d46
phanindra-pds <phanindra@Privacydatasystems.com>
2022-02-04 13:00:09 +0530

    desktop view only

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,7 @@ import { Provider } from "react-redux";
 import { store } from "../redux/store";
 import { debounce } from "debounce";
 import saveToLocalStorage from "../localStorage/saveToLocalStorage";
+import { useWindowSize } from "react-use";
 
 store.subscribe(
   debounce(() => {
@@ -11,10 +12,18 @@ store.subscribe(
 );
 
 function MyApp({ Component, pageProps }) {
-  
+  const windowSize = useWindowSize();
+  console.log(windowSize);
   return (
     <Provider store={store}>
-      <Component {...pageProps} />
+      {windowSize.width <= 960 ? (
+          <div className="responsive">
+            <h4>Website currently works only on Desktop view</h4>
+          </div>
+        ) : (
+          <Component {...pageProps} />	
+        )}
+        {/* <Component {...pageProps} /> */}
     </Provider>
   );
 }
